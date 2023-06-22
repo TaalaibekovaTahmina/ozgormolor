e6f7ce7792666dfdfccb173d099838b5d171e686
TaalaibekovaTahmina <tahtaalaibekova@gmail.com>
2023-06-22 21:36:57 +0600

    ozgormolor

---
+++ bin/cabak1_dart.dart
@@ -0,0 +1,29 @@
+void main() {
+  var text = 'Dart sabak';
+  var san = 12;
+  var bool1 = true;
+
+  String text1 = 'Tahmina';
+  String text2 = '100';
+  String text3 = 'sabak';
+
+  int san1 = 100;
+  int san2 = -12;
+  int san3 = 87;
+
+  double endP = 12;
+  double endP1 = -25.45;
+  double endP2 = 12.34;
+
+  num dart = 11.56;
+  num dart1 = 30;
+  num dart2 = -56;
+
+  bool sabak = true;
+  bool sabak3 = false;
+
+  dynamic box = 'narun';
+  dynamic box1 = 30;
+  dynamic box2 = false;
+  dynamic box3 = -23.21;
+}
---
+++ test/cabak1_dart_test.dart
@@ -0,0 +1,8 @@
+import 'package:cabak1_dart/cabak1_dart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
